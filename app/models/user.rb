f40e1a1f1e8009a169021b9a115e4a7c5e69f768
Kai Huang <kaihuang@kais-air.attlocal.net>
2016-02-15 02:12:06 -0800

    Validate user name and email in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+	validates :name, presence: true, length: {maximum: 50}
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: {maximum:L 255},
+		format: { with: VALID_EMAIL_REGEX },
+		uniqueness: true {case_sensitive: false}
 end
