4be868f540c9574cbd7578f85dc617f0742e94e5
Kai Huang <kaihuang@kais-air.attlocal.net>
2016-02-15 00:54:41 -0800

    Remove comments and modify welcome index

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 Rails.application.routes.draw do
   get 'users/new'
-
   get 'welcome/index'
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
   root 'welcome#index'
   resources :articles do
     resources :comments
   end
-  get 'about'=>'welcome#about'
-  get 'contact'=>'welcome#contact'
+  get 'about' => 'welcome#about'
+  get 'contact' => 'welcome#contact'
+  get 'signup' => 'users#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
