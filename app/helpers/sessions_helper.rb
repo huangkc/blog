b06b8beeffa5cc328183090399bd9616a40c8879
Kai Huang <kaihuang@kais-air.attlocal.net>
2016-02-16 11:38:50 -0800

    Store and redirect back to user location

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,7 +15,7 @@ module SessionsHelper
   def current_user?(user)
     user == current_user
   end
-  
+
   # Returns the current logged-in user (if any).
   def current_user
     if (user_id = session[:user_id])
@@ -47,4 +47,14 @@ module SessionsHelper
     @current_user = nil
   end
 
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
 end
