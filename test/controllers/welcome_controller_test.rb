bf0cbb4b439156c7ca7d847689e4a4654a49d03c
Kai Huang <kaihuang@kais-air.attlocal.net>
2016-02-18 12:51:46 -0800

    Add integration tests

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -5,5 +5,14 @@ class WelcomeControllerTest < ActionController::TestCase
     get :index
     assert_response :success
   end
-
+	
+	test "should get about" do
+    get :about
+    assert_response :success
+  end
+	
+	test "should get contact" do
+    get :contact
+    assert_response :success
+  end
 end
