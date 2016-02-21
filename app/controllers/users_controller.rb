4e8a0f5fac4403faafb80e3936361fb938c55873
Kai Huang <kaihuang@kais-air.attlocal.net>
2016-02-21 13:05:46 -0800

    Send mail to new user after signining up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,9 +20,12 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)
 
   	if @user.save 
-      log_in @user
-  		flash[:success] = "Welcome!"
-    	redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
+    #   log_in @user
+  		# flash[:success] = "Welcome!"
+    # 	redirect_to @user
     else
       render 'new'
     end
