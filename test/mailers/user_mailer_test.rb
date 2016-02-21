8098b928bf79c14312736b3395da9d22156e25dd
Kai Huang <kaihuang@kais-air.attlocal.net>
2016-02-21 12:58:00 -0800

    Write tests for user actional mailer

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -2,10 +2,12 @@ require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
   test "account_activation" do
-    mail = UserMailer.account_activation
+    user = users(:michael)
+    user.activation_token = User.new_token
+    mail = UserMailer.account_activation(user)
     assert_equal "Account activation", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
+    assert_equal ["michael@example.com"], mail.to
+    assert_equal ["noreply@example.com"], mail.from
     assert_match "Hi", mail.body.encoded
   end
 
@@ -13,7 +15,7 @@ class UserMailerTest < ActionMailer::TestCase
     mail = UserMailer.password_reset
     assert_equal "Password reset", mail.subject
     assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
+    assert_equal ["noreply@example.com"], mail.from
     assert_match "Hi", mail.body.encoded
   end
 
