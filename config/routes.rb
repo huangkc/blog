ac28ee9de58a928115da6945a1df2a2947a0dff8
Kai Huang <kaihuang@kais-air.attlocal.net>
2016-02-21 14:06:40 -0800

    Add email reset form

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   end
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
